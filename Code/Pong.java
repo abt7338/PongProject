8f4d8a3e0e4a325893cb41b742ffa9209e6adce4
abt7338 <abt7338@g.rit.edu>
2016-01-13 00:03:18 -0600

    Better setup

--- Code/Pong.java
+++
@@ -1,7 +0,0 @@
-public class Pong
-{
-    public static void main(String[] args)
-    {
-
-    }
-}
---
+++ Pong/src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Alex on 1/13/2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
